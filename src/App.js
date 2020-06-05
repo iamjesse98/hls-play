82a3e7dc3437186615c79141c5786c386b9e7b6a
Yadagalla Jaswanth Kumar <yadagallajaswanthkumar98@gmail.com>
2020-06-05 17:33:29 +0530

    vid
    player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactHLS from "react-hls-player";
+import { Label, Input } from "@rebass/forms";
+import { Box } from "rebass";
 import "./styles.css";
 
-export default function App() {
+export default () => {
+  const [vidUrl, setVidUrl] = useState("");
+  const [name, setName] = useState("untitled");
+
+  useEffect(() => {
+    document.title = name;
+  });
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {vidUrl && <ReactHLS url={vidUrl} />}
+      <Box>
+        <Label htmlFor="name">Name</Label>
+        <Input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+
+        <Label htmlFor="url">url</Label>
+        <Input
+          id="url"
+          name="url"
+          type="url"
+          placeholder="URL"
+          value={vidUrl}
+          onChange={e => setVidUrl(e.target.value)}
+        />
+      </Box>
     </div>
   );
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider } from "emotion-theming";
+import theme from "@rebass/preset";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   rootElement
 );
